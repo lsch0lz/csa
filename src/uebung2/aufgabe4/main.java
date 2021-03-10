d5a12a572e18bee2e96fa529c3c332bf874f06d4
italyboy2012 <giuseppe@local>
2021-03-10 20:20:42 +0100

    Aufgabe 4 begonnen

---
+++ src/uebung2/aufgabe4/main.java
@@ -0,0 +1,22 @@
+package uebung2.aufgabe4;
+
+/**
+ * Dokumentation:
+ * https://commons.apache.org/proper/commons-net/javadocs/api-3.6/org/apache/commons/net/ntp/NTPUDPClient.html#NTPUDPClient()
+ *
+ * Beispielimplementation:
+ * https://commons.apache.org/proper/commons-net/examples/ntp/NTPClient.java
+ *
+ * Vorgehen:
+ * 
+ *
+ */
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
