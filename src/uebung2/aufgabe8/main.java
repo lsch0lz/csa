50a86b7caa4ca7342e974bc23b6db3eb60021790
therealgherkhin <lscholz14@googlemail.com>
2021-03-16 11:51:45 +0100

    Aufgabe 8 beendet

--- src/uebung2/aufgabe8/main.java
+++ src/uebung2/aufgabe8/main.java
@@ -1,42 +1,13 @@
 package uebung2.aufgabe8;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import uebung2.aufgabe8.Server;
 
 public class main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/index.html", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
+        Thread serverThread = new Thread(new Server());
+
+        serverThread.start();
+
     }
-
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "<title>Page Title</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "\n" +
-                    "<h1>My First Heading</h1>\n" +
-                    "<p>My first paragraph.</p>\n" +
-                    "\n" +
-                    "</body>\n" +
-                    "</html>";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
-
 }
