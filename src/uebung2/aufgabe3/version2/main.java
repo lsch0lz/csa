7fbcf224cd517607b83fc1f1572c0e3edf119e62
italyboy2012 <giuseppe@local>
2021-03-16 12:26:47 +0100

    Aufgabe 3 erweitert

--- src/uebung2/aufgabe3/version2/main.java
+++ src/uebung2/aufgabe3/version2/main.java
@@ -1,4 +1,57 @@
 package uebung2.aufgabe3.version2;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
 public class main {
+    private static String prefix = ">>> ";
+
+    public static void main(String[] args) {
+        ArrayList<String> serverList = new ArrayList<>();
+        serverList.add("time.fu-berlin.de");
+        serverList.add("zeit.fu-berlin.de");
+
+        for (int i = 0; i < serverList.size(); i++) {
+            getTime(serverList.get(i));
+        }
+
+    }
+
+    /**
+     * "Die folgende Implementierung zeigt, wie ein Zeit-Server angesprochen wird.
+     * Dazu wird zunächst ein leeres Paket als Anfrage an den Server gesendet.
+     * Anschließend wird mit receive() auf das hereinkommende Paket gewartet.
+     * Wenn dieses kommt, dann liefert getData() eine Zeichenkette mit der Zeit."
+     *
+     * Siehe http://www.tutego.de/java/articles/Java-Zeitdienste.html
+     */
+    public static void getTime(String server) {
+        System.out.println(prefix + "Anfrage senden an: " + server);
+        try {
+            DatagramPacket packet;
+            // Anfrage an Server senden
+            byte data[] = new byte[1024];
+            InetAddress ia = InetAddress.getByName(server);
+            packet = new DatagramPacket(data, data.length, ia, 13);
+            DatagramSocket toSocket = new DatagramSocket();
+            toSocket.send(packet);
+            System.out.println(prefix + "'-> Anfrage gesendet.");
+            // Antwort des Servers abwarten
+            DatagramSocket fromSocket = toSocket;
+            //new DatagramSocket();
+            packet = new DatagramPacket(data, data.length);
+            System.out.println(prefix + "'-> Auf Antwort warten.");
+            fromSocket.receive(packet);
+            System.out.println(prefix + "Antwort empfangen:");
+            String s = prefix + "'-> Server: " + packet.getAddress() +
+                    ":" + packet.getPort() +
+                    " => " + new String(packet.getData());
+            System.out.println(s);
+            fromSocket.close();
+        } catch (Exception e) {
+            System.out.println(prefix + "Fehler: " + e.toString());
+        }
+    }
 }
