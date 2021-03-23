fd108a07349204482af4f0e36becaa1bf0eb9807
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2021-03-23 10:54:39 +0100

    Delete secrets.java

--- src/uebung3/secrets.java
+++
@@ -1,7 +0,0 @@
-package uebung3;
-
-public interface secrets {
-    String url = "jdbc:mysql://lukasscholz.de:3306/d035dbd4?useSSL=false&serverTimezone=Europe/Warsaw";
-    String user = "d035dbd4";
-    String password = "uN5s45ppG2G3YwJ5";
-}
