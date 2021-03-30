bf5711a575c566356122e23f8959141dfddf70e4
italyboy2012 <giuseppe@local>
2021-03-30 13:17:32 +0200

    Added DTO 'Kunde'

---
+++ src/uebung3/DTO/Kunde.java
@@ -0,0 +1,74 @@
+package uebung3.DTO;
+
+public class Kunde {
+    private final long kID;
+    private String name;
+    private String vorname;
+    private int plz;
+    private String ort;
+    private String land;
+    private String strasseHnr;
+
+    public Kunde(long kID, String name, String vorname, int plz, String ort, String land, String strasseHnr) {
+        this.kID = kID;
+        this.name = name;
+        this.vorname = vorname;
+        this.plz = plz;
+        this.ort = ort;
+        this.land = land;
+        this.strasseHnr = strasseHnr;
+    }
+
+    public long getkID() {
+        return kID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public void setPlz(int plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public String getLand() {
+        return Land;
+    }
+
+    public void setLand(String land) {
+        Land = land;
+    }
+
+    public String getStrasseHnr() {
+        return strasseHnr;
+    }
+
+    public void setStrasseHnr(String strasseHnr) {
+        this.strasseHnr = strasseHnr;
+    }
+
+}
