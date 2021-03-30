170bb4b754c18a4cf37d38c75e303858e029cde8
italyboy2012 <giuseppe@local>
2021-03-30 21:49:22 +0200

    integrated DTOs to DBFunctions

--- src/uebung3/DB/DBFunctions.java
+++ src/uebung3/DB/DBFunctions.java
@@ -30,7 +30,6 @@ public class DBFunctions implements secrets {
         return conn;
     }
 
-    // Store DB-Content to DTO ---------------------------------------
     //select
     public static void getKunden(ArrayList<Kunde> kunden) throws  SQLException {
         Connection conn = conn();
@@ -52,7 +51,6 @@ public class DBFunctions implements secrets {
         conn.close();
     }
 
-    //select
     public static void getBestellungen(ArrayList<Bestellung> bestellungen) throws  SQLException {
         Connection conn = conn();
         String query = "SELECT * FROM Bestellung";
@@ -68,7 +66,6 @@ public class DBFunctions implements secrets {
         conn.close();
     }
 
-    //select
     public static void getBeinhaltet(ArrayList<Beinhaltet> beinhaltet) throws  SQLException {
         Connection conn = conn();
         String query = "SELECT * FROM Beinhaltet";
@@ -84,7 +81,6 @@ public class DBFunctions implements secrets {
         conn.close();
     }
 
-    //select
     public static void getArtikel(ArrayList<Artikel> artikel) throws  SQLException {
         Connection conn = conn();
         String query = "SELECT * FROM Artikel";
@@ -101,42 +97,94 @@ public class DBFunctions implements secrets {
         conn.close();
     }
 
-
-
-
-
-
-
-
-
-
     //insert Statement
-    public static void insertKunde(String name, String vorname, int plz, String ort, String land, String strassehnr) throws SQLException {
+    public static void insertKunde(Kunde kunde) throws SQLException {
         Connection conn = conn();
-        String insert = "INSERT INTO Kunde (Name, Vorname, PLZ, Ort, Land, StrasseHnr) VALUES ('" + name + "', '" + vorname + "', '" + plz +"', '" + ort + "', '" + land + "', '" + strassehnr + "');";
-
+        String insert = "INSERT INTO Kunde (Name, Vorname, PLZ, Ort, Land, StrasseHnr) VALUES ('" + kunde.getName() + "', '" + kunde.getVorname() + "', '" + kunde.getPlz() +"', '" + kunde.getOrt() + "', '" + kunde.getLand() + "', '" + kunde.getStrasseHnr() + "');";
         Statement stmt = conn.createStatement();
         stmt.executeUpdate(insert);
-        print("Insert Statement");
+        conn.close();
+    }
+
+    public static void insertBestellung(Bestellung bestellung) throws SQLException {
+        Connection conn = conn();
+        String insert = "INSERT INTO Bestellung (B-ID, K-ID) VALUES ('" + bestellung.getbID() + "', '" + bestellung.getkID() + "');";
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(insert);
+        conn.close();
+    }
+
+    public static void insertBeinhaltet(Beinhaltet beinhaltet) throws SQLException {
+        Connection conn = conn();
+        String insert = "INSERT INTO Beinhaltet (A-ID, B-ID) VALUES ('" + beinhaltet.getaID() + "', '" + beinhaltet.getbID() + "');";
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(insert);
+        conn.close();
+    }
+
+    public static void insertArtikel(Artikel artikel) throws SQLException {
+        Connection conn = conn();
+        String insert = "INSERT INTO Artikel (A-ID, Bezeichnung, Preis) VALUES ('" + artikel.getaID() + "', '" + artikel.getBezeichnung() + "', '" + artikel.getPreis() +"');";
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(insert);
         conn.close();
     }
 
     //insert PreparedStatement
-    public static void insertKundePS(String name, String vorname, int plz, String ort, String land, String strassehnr) throws SQLException {
+    public static void insertKundePS(Kunde kunde) throws SQLException {
         Connection conn = conn();
         String insert = "INSERT INTO Kunde (Name, Vorname, PLZ, Ort, Land, StrasseHnr) VALUES (?, ?, ?, ?, ?, ?)";
 
         PreparedStatement statement = conn.prepareStatement(insert);
 
-        statement.setString(1, name);
-        statement.setString(2, vorname);
-        statement.setInt(3, plz);
-        statement.setString(4, ort);
-        statement.setString(5, land);
-        statement.setString(6, strassehnr);
+        statement.setString(1, kunde.getName());
+        statement.setString(2, kunde.getVorname());
+        statement.setInt(3, kunde.getPlz());
+        statement.setString(4, kunde.getOrt());
+        statement.setString(5, kunde.getLand());
+        statement.setString(6, kunde.getStrasseHnr());
 
         statement.executeUpdate();
-        print("Insert Prepared Statement");
+        conn.close();
+    }
+
+    public static void insertBestellungPS(Bestellung bestellung) throws SQLException {
+        Connection conn = conn();
+        String insert = "INSERT INTO Bestellung (B-ID, K-ID) VALUES (?, ?)";
+
+        PreparedStatement statement = conn.prepareStatement(insert);
+
+        statement.setLong(1, bestellung.getbID());
+        statement.setLong(2, bestellung.getkID());
+
+        statement.executeUpdate();
+        conn.close();
+    }
+
+    public static void insertBeinhaltetPS(Beinhaltet beinhaltet) throws SQLException {
+        Connection conn = conn();
+        String insert = "INSERT INTO Beinhaltet (A-ID, B-ID) VALUES (?, ?)";
+
+        PreparedStatement statement = conn.prepareStatement(insert);
+
+        statement.setLong(1, beinhaltet.getaID());
+        statement.setLong(2, beinhaltet.getbID());
+
+        statement.executeUpdate();
+        conn.close();
+    }
+
+    public static void insertArtikelPS(Artikel artikel) throws SQLException {
+        Connection conn = conn();
+        String insert = "INSERT INTO Artikel (A-ID, Bezeichnung, Preis) VALUES (?, ?, ?)";
+
+        PreparedStatement statement = conn.prepareStatement(insert);
+
+        statement.setLong(1, artikel.getaID());
+        statement.setString(2, artikel.getBezeichnung());
+        statement.setDouble(3, artikel.getPreis());
+
+        statement.executeUpdate();
         conn.close();
     }
 
@@ -144,20 +192,73 @@ public class DBFunctions implements secrets {
     public static void emptyTable(String table) throws SQLException {
         Connection conn = conn();
 
-        String delete = "DELETE FROM " + "`" + table + "`;";
+        String delete = "DELETE FROM `" + table + "`;";
         Statement stmt = conn.createStatement();
         stmt.execute(delete);
 
         conn.close();
+    }
 
-        print("Leere Tabelle '" + table + "'");
+    //update Statement
+    public static void updateEntity(Object entity) throws Exception {
+        if(entity.getClass().getName().equals("Kunde")) {
+
+        } else if(entity.getClass().getName().equals("Bestellung")) {
+
+        } else if(entity.getClass().getName().equals("Beinhaltet")) {
+
+        } else if(entity.getClass().getName().equals("Artikel")) {
+
+        } else {
+            throw new Exception("Keine gültige Entität übergeben!");
+        }
+    }
+
+
+    //update Statement
+    public static void updateTable(String table, String changingColumn, String changingValue, String column, String value) throws SQLException {
+        Connection conn = conn();
+
+        String update = "UPDATE "  + table  + " SET " +  changingColumn + "= " + "'" + changingValue + "'" + " WHERE " + column + "= " + "'" + value + "'" + ";";
+        Statement stmt = conn.createStatement();
+        print("Verändere Tabelle '" + table + "'.");
+        stmt.execute(update);
+        print("Update Statement");
+        conn.close();
+
+
 
     }
+
+    //update PreparedStatement
+    public static void updateTablePS(String table, String changingColumn, String changingValue, String column, String value) throws SQLException {
+
+        Connection conn = conn();
+
+        PreparedStatement ps = conn.prepareStatement("UPDATE Kunde SET Name= ? WHERE Vorname= ?;");
+
+        ps.setString(1, changingValue);
+        ps.setString(2, value);
+
+        print("Verändere Tabelle '" + table + "'.");
+
+        ps.executeUpdate();
+        ps.close();
+        print("Update PreparedStatment");
+        conn.close();
+    }
+
+
+
+
+
+
+
 
     public static void selectKunde(String name) throws SQLException{
         Connection conn = conn();
 
-        String select = "SELECT * FROM Kunde WHERE Name= " + "'" + name + "';";
+        String select = "SELECT * FROM Kunde WHERE Name= '" + name + "';";
         Statement statement = conn.createStatement();
 
         print("Gebe Kunden: " + name + " aus");
